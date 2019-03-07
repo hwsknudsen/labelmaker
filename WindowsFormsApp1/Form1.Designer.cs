f24a6741daa80159afec5422242448806e98e0b9
Henrik Knudsen <Henrik@viking.bm>
2019-03-07 09:21:55 -0400

    add texted

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -102,9 +102,9 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(340, 188);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 31);
+            this.label3.Size = new System.Drawing.Size(87, 31);
             this.label3.TabIndex = 6;
-            this.label3.Text = "99.9";
+            this.label3.Text = "99.90";
             // 
             // textBox2
             // 
